78d77b9d8113cfa48d441a4a378f86b59a58c6e8
Milica Vujić <milicavujic2001@gmail.com>
2024-07-03 18:52:25 +0200

    feat: Cards for movies using Material UI added.

--- movie_web_app/ClientApp/src/components/Home.js
+++ movie_web_app/ClientApp/src/components/Home.js
@@ -1,50 +1,84 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Link } from 'react-router-dom';
+import './App.css'; 
 
 export class Home extends Component {
     static displayName = Home.name;
 
     constructor(props) {
         super(props);
-        this.state = { movies: [], loading: true };
+        this.state = {
+            movies: [],
+            loading: true
+        };
     }
 
     componentDidMount() {
         this.populateMoviesData();
     }
 
-    static renderMoviesTable(movies) {
+    toggleFavorite = (movieId) => {
+        const updatedMovies = this.state.movies.map(movie => {
+            if (movie.id === movieId) {
+                return { ...movie, favorite: !movie.favorite };
+            }
+            return movie;
+        });
+        this.setState({ movies: updatedMovies });
+    }
+
+    renderMoviesCards = () => {
+        const { movies } = this.state;
+
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Genre</th>
-                        <th>Duration (min)</th>
-                        <th>Year</th>
-                        <th>Rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {movies.map(movie =>
-                        <tr key={movie.id}>
-                            <td>{movie.title}</td>
-                            <td>{movie.genre}</td>
-                            <td>{movie.duration}</td>
-                            <td>{movie.year}</td>
-                            <td>{movie.rating}</td>
-                            <td>    <img src={movie.coverImage} alt="Cover image" style={{ width: '5em', height: '5em' }}></img></td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+            <Grid container spacing={3}>
+                {movies.map(movie => (
+                    <Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
+                        <Link to={`/movie/${movie.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                            <Card className="movie-card">
+                                <CardMedia
+                                    component="img"
+                                    height="200"
+                                    image={movie.coverImage}
+                                    alt="Cover image"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {movie.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Genre: {movie.genre}<br />
+                                        Duration: {movie.duration} min<br />
+                                        Year: {movie.year}<br />
+                                        Rating: {movie.rating}
+                                    </Typography>
+                                    <IconButton
+                                        onClick={() => this.toggleFavorite(movie.id)}
+                                        color={movie.favorite ? 'secondary' : 'default'}
+                                    >
+                                        {movie.favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                                    </IconButton>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </Grid>
+                ))}
+            </Grid>
         );
     }
 
     render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : Home.renderMoviesTable(this.state.movies);
+        const { loading } = this.state;
+        const contents = loading ? <p><em>Loading...</em></p> : this.renderMoviesCards();
 
         return (
             <div>
@@ -59,7 +93,9 @@ export class Home extends Component {
         try {
             const response = await axios.get('https://localhost:7004/api/movie');
             console.log('Movies data:', response.data);
-            this.setState({ movies: response.data, loading: false });
+            // Dodajemo polje 'favorite' u svaki film
+            const moviesWithFavorites = response.data.map(movie => ({ ...movie, favorite: false }));
+            this.setState({ movies: moviesWithFavorites, loading: false });
         } catch (error) {
             console.error('There was an error!', error);
         }
--- movie_web_app/ClientApp/src/components/NavMenu.js
+++ movie_web_app/ClientApp/src/components/NavMenu.js
@@ -6,25 +6,39 @@ import './NavMenu.css';
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.toggleTheme = this.toggleTheme.bind(this);
+    
     this.state = {
-      collapsed: true
+      collapsed: true,
+      theme: 'light' // Početna tema je svijetla
     };
   }
 
-  toggleNavbar () {
+  toggleNavbar() {
     this.setState({
       collapsed: !this.state.collapsed
     });
   }
 
+  toggleTheme() {
+    const newTheme = this.state.theme === 'light' ? 'dark' : 'light';
+    this.setState({ theme: newTheme });
+
+    // Ovdje možete postaviti globalni state ili koristiti Redux za promjenu teme u cijeloj aplikaciji
+    // Primjer kako bi moglo izgledati postavljanje teme u globalnom Redux state-u:
+    // this.props.setTheme(newTheme);
+  }
+
   render() {
+    const { theme } = this.state;
+
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+        <Navbar className={`navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3 ${theme === 'dark' ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`} light>
           <NavbarBrand tag={Link} to="/">movie_web_app</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
@@ -37,6 +51,9 @@ export class NavMenu extends Component {
               </NavItem>
             </ul>
           </Collapse>
+          <button className="btn btn-sm btn-outline-secondary ml-auto" onClick={this.toggleTheme}>
+            Toggle Theme
+          </button>
         </Navbar>
       </header>
     );
